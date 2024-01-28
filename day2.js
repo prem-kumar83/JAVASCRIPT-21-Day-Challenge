62b54d12cfa148dcc2488bd9537afccbb5686cc9
prem-kumar83 <premkumar8312@gmail.com>
2024-01-28 22:59:48 +0530

    completed task3

--- day2.js
+++ day2.js
@@ -4,7 +4,7 @@ let a=2;
 let b=5;
 let fullName=firstName.concat(lastName);
 
-console.log(fullName);
-console.log(a+b);
-console.log(a==b);
+console.log(fullName);      // perform concatation
+console.log(a+b);           //perform arithmetic operation
+console.log(a==b);          // perform boolean operation
 
---
+++ day3.js
@@ -0,0 +1,9 @@
+let number=876;
+if(number%2==0)
+{
+    console.log("Even Number");
+}
+else
+{
+console.log("Odd Number");
+}
