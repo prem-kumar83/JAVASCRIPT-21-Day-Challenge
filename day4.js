18bd0a03d5952f3b29d4212927387b05a3cc1954
prem-kumar83 <premkumar8312@gmail.com>
2024-01-29 22:44:53 +0530

    complete challenge day4

---
+++ day4.js
@@ -0,0 +1,14 @@
+console.log("Output using for loop");
+for(let count=1; count<10; count++)
+
+{
+    console.log(count);
+}
+let i=1;
+console.log("Output using while loop");
+while(i<=10)
+{   
+    console.log(i);
+    i++;
+}
+
